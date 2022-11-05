eef6d3057332f9d110be072efcbe077890589ba4
elliem <rogo.mogo1999@gmail.com>
2022-11-05 11:38:16 +0100

    v1.0

---
+++ WriteIMG.py
@@ -0,0 +1,51 @@
+from PIL import Image,ImageDraw
+Out = open('Out.txt', 'r')
+Color = open("color.txt", "r")
+Size = open("Size.txt", "r")
+
+
+
+ColorList = []
+for line in Color:
+    ColorRow = line.split("|")
+    ColorRow.pop(-1)
+    for x in range(0,len(ColorRow)):
+        ColorRow[x] = ColorRow[x].split(".")
+    ColorList.append(ColorRow)
+
+LineList = []
+for line in Out:
+    line = line.replace("\n", "")
+    LineList.append(line)
+
+width = int(Size.readline())
+height = int(Size.readline())
+
+
+Img = Image.new("RGB", (6*width-5, 10*height-23), color=(100,100,100))
+d = ImageDraw.Draw(Img)
+
+def DrawText(Row,Pixel,char,colour):
+    d.text(((Pixel-1)*6, (Row - 1) * 10-3), char, fill=colour)
+
+
+def GetCharTXT(Row, Pixel):
+    if Pixel >= width - 1:
+        Pixel = width - 2
+    Line = LineList[Row - 1]
+    List = Line.split("|")
+    List.pop(-1)
+    return List[Pixel]
+
+for Row in range(1, height):
+    color = ColorList[Row-1]
+    print(Row)
+    for Pixel in range(1, width-1):
+        r = int(color[Pixel][0])
+        g = int(color[Pixel][1])
+        b = int(color[Pixel][2])
+        DrawText(Row, Pixel, GetCharTXT(Row, Pixel),(r,g,b))
+
+
+
+Img.save("Bild.png")
