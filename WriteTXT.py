8debce55ff839a33548e0b5e64ba34056f6c8e8f
elliem <rogo.mogo1999@gmail.com>
2022-11-06 16:10:32 +0100

    Huren Comments

--- WriteTXT.py
+++ WriteTXT.py
@@ -1,58 +1,63 @@
 from PIL import Image
 import os
+
+# Open Files
+# ---------------------------------------#
+Out = open("Out.txt", "w+")
+Color = open("color.txt", "w+")
+Conf = open("Config.txt", "r")
+Size = open("Size.txt", "w")
+# ---------------------------------------#
+
+# Read Config
+# ------------------------------------- #
 Config = []
-Conf = open("Config.txt","r")
 for lines in Conf:
     parameter = (lines.split(":"))[1]
     Config.append(parameter)
 Config[1] = Config[1].split(",")
 
-for x in range(0,len(Config[1])):
+for x in range(0, len(Config[1])):
     List = Config[1]
     List[x] = List[x].replace('"', "")
     List[x] = List[x].replace("\n", "")
 Config[1] = List
+# ------------------------------------ #
 
-
-Big = float(Config[0])
-size = [round(50 * Big), round(25 * Big)]
-Size = open("Size.txt", "w")
-# Img
-# ---------------------------------------#
+# Get Image in Dir
+# -------------------------------------------------- #
 NameList = os.listdir("./")
-
-
 for x in NameList:
     if x.__contains__(".png") and x != "Bild.png":
         print(x)
         PNG = x
         break
+# -------------------------------------------------- #
+
+# Pillow Picturure
+# --------------------------------------- #
+Big = float(Config[0])
+size = [round(50 * Big), round(25 * Big)]
 Img = Image.open(PNG)
 Img = Img.resize(size)
 width = Img.size[0]
 height = Img.size[1]
-Size.write(str(width)+"\n")
-Size.write(str(height))
 px = Img.load()
-# ---------------------------------------#
+# --------------------------------------- #
 
-# TXT
-# ---------------------------------------#
-Out = open("Out.txt", "w+")
-Color = open("color.txt", "w+")
-# ---------------------------------------#
+# Write to Size
+# -------------------------- #
+Size.write(str(width) + "\n")
+Size.write(str(height))
+# -------------------------- #
 
-
-
-
-
-
+# Char-List
+# ---------------------------------- #
 CharList = Config[1]
 if Config[2] == "true\n":
     CharList.reverse()
-
 divisor = 255 / (len(CharList) - 1)
-
+# ---------------------------------- #
 
 def GetBrightness(Pixel):
     R = Pixel[0]
@@ -75,21 +80,15 @@ def WriteToTxT(RowList):
         Out.write(x + "|")
     Out.write("\n")
 
-def WriteColor (ColorList):
+
+def WriteColor(ColorList):
     Color.write("|")
     for x in ColorList:
-        Color.write(str(x[0])+".")
+        Color.write(str(x[0]) + ".")
         Color.write(str(x[1]) + ".")
         Color.write(str(x[2]))
         Color.write("|")
     Color.write("\n")
-
-
-
-
-
-
-
 
 
 # Get mult
@@ -103,10 +102,10 @@ for Row in range(1, height):
             min = PixelBrightness
         if PixelBrightness > max:
             max = PixelBrightness
-if max-min !=0:
- mult = 255 / (max - min)
+if max - min != 0:
+    mult = 255 / (max - min)
 else:
- mult = 1
+    mult = 1
 # ------------------------------#
 
 # Txt File Out.txt
@@ -117,7 +116,7 @@ for Row in range(1, height):
     for Pixel in range(1, width):
         PixelBrightness = GetBrightness(px[Pixel, Row])
         RowList += GetChar(PixelBrightness)
-        ColorRow += [px[Pixel,Row]]
+        ColorRow += [px[Pixel, Row]]
     WriteToTxT(RowList)
     WriteColor(ColorRow)
 # -------------------------------#
